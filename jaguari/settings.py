4ad6e24a5156172418affbc85f89ad1c97d18d8f
Ranieri Althoff <ranisalt@gmail.com>
2016-11-21 03:29:34 -0200

    Disable django user model

--- jaguari/settings.py
+++ jaguari/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    # 'django.contrib.auth.backends.ModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
     'django_cas_ng.backends.CASBackend',
 )
 
