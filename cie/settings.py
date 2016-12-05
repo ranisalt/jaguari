f89047b22b523e769b7889a427bf377a59ebf75c
Ranieri Althoff <ranisalt@gmail.com>
2016-12-05 18:48:26 -0200

    Replace SQLite with MySQL

--- cie/settings.py
+++ cie/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'cie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
     }
 }
 
