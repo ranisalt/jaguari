f06859aa9ca97abe61edd237d72af256d5e341dd
Ranieri Althoff <ranisalt@gmail.com>
2016-12-16 21:32:51 -0200

    Show all needed data on order change page

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Degree, Order
 
 
@@ -10,13 +11,62 @@ class DegreeAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    def picture_tag(self, obj):
+    def birthday_tag(self, obj: Order):
+        return obj.birthday.strftime('%d/%m/%Y')
+
+    birthday_tag.short_description = 'Data de nascimento'
+
+    def cpf_tag(self, obj: Order):
+        parts = (obj.cpf[i:i+3] for i in range(0, 11, 3))
+        return '{}.{}.{}-{}'.format(*parts)
+
+    cpf_tag.short_description = 'CPF'
+
+    def degree_tag(self, obj: Order):
+        return '{} em {}'.format(obj.degree.get_tier_display(), obj.degree.name)
+
+    degree_tag.short_description = 'Escolaridade'
+
+    def name_tag(self, obj: Order):
+        return obj.student.get_full_name()
+
+    name_tag.short_description = 'Nome'
+
+    def identity(self, obj: Order):
+        return '{} {}/{}'.format(obj.identity_number,
+                                 obj.identity_issuer,
+                                 obj.identity_state)
+
+    identity.short_description = 'RG'
+
+    picture_html = r'<img src="{}" style="max-height: 200px; max-width: ' \
+                   r'200px;"/>'
+
+    def picture_tag(self, obj: Order):
         import os
         from django.utils.safestring import mark_safe
         if obj.picture:
-            return mark_safe('<img src="{}" />'.format(
-                os.path.join('/', settings.MEDIA_URL, obj.picture.url)
-            ))
-        return '(no picture)'
+            return mark_safe(
+                self.picture_html.format(
+                    os.path.join('/', settings.MEDIA_URL, obj.picture.url)
+                ))
 
-    list_display = ['student', 'picture_tag']
+    picture_tag.short_description = 'Imagem'
+
+    list_display = ('name_tag', 'created_at', 'print_status')
+    fields = ('use_code',
+              'name_tag',
+              'birthday_tag',
+              'cpf_tag',
+              'identity',
+              'enrollment_number',
+              'degree_tag',
+              'picture_tag',
+              'print_status')
+    readonly_fields = ('name_tag',
+                       'birthday_tag',
+                       'cpf_tag',
+                       'identity',
+                       'enrollment_number',
+                       'degree_tag',
+                       'picture_tag')
