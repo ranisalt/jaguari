187e7cbee77938bb2c3ddcd7fae0b780cbf99917
Ranieri Althoff <ranisalt@gmail.com>
2016-12-19 20:33:48 -0200

    Improve homepage and navbar styling

--- cie/urls.py
+++ cie/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^orders/', include('orders.urls')),
