80e2181cc3951ab857a11c35bced700f123c9e4d
Ranieri Althoff <ranisalt@gmail.com>
2016-12-21 03:11:10 -0200

    Add type hinting for stricter testing
    Former-commit-id: 3bb4fe3415cdb36fbc70f9af43b204e54b3de1f5

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import View
 from pagseguro.api import PagSeguroApi, PagSeguroItem
@@ -8,7 +9,7 @@ from .models import Order
 class OrdersView(LoginRequiredMixin, View):
     model = Order
 
-    def get(self, request):
+    def get(self, request: HttpRequest) -> HttpResponse:
         """Index of user orders"""
         queryset = self.model.objects.filter(student=self.request.user)
         return render(request, 'orders/index.html', {
@@ -19,7 +20,7 @@ class OrdersView(LoginRequiredMixin, View):
 class OrderDetailView(View):
     model = Order
 
-    def get(self, request):
+    def get(self, request: HttpRequest) -> HttpResponse:
         """Show a single order"""
         pass
 
@@ -27,14 +28,14 @@ class OrderDetailView(View):
 class OrderCreateView(LoginRequiredMixin, View):
     model = Order
 
-    def get(self, request):
+    def get(self, request: HttpRequest) -> HttpResponse:
         order = self.model.objects.fetch(request.user)
         request.session['order'] = str(order.pk)
         return render(request, 'orders/new.html', {
             'order': order
         })
 
-    def post(self, request):
+    def post(self, request: HttpRequest) -> HttpResponse:
         """Create new order"""
         order = get_object_or_404(self.model, pk=request.session['order'])
         order.picture = request.FILES['picture']
