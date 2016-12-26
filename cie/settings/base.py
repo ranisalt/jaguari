7e1c81ff9cb803cb9b036a9bb01e074ce5bc781e
Ranieri Althoff <ranisalt@gmail.com>
2016-12-26 01:15:47 -0200

    Split configuration for development & production

--- cie/settings/base.py
+++ cie/settings/base.py
@@ -10,23 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
+from os import path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'knmo&y7or4!sxfqab74_-923od*rih36f5!2=f_#x!t%pjkdoc'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['cie.dce.ufsc.br']
-
+# Build paths inside the project like this: path.join(BASE_DIR, ...)
+BASE_DIR = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
 # Application definition
 
@@ -63,7 +50,7 @@ ROOT_URLCONF = 'cie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,42 +66,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cie.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
 
-LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+LOCALE_PATHS = [path.join(BASE_DIR, 'locale')]
 
 TIME_ZONE = 'America/Sao_Paulo'
 
@@ -127,29 +84,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [path.join(BASE_DIR, 'staticfiles')]
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'public')
-
 STATIC_URL = '/static/'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# File upload configuration
-MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
-
 # Static file compression
 COMPRESS_PRECOMPILERS = [
-    ('text/x-scss', 'sassc -t compressed {infile} > {outfile}'),
+    ('text/x-scss', 'sass --scss {infile} {outfile}'),
 ]
-
-# Student database credentials
-CAGR_KEY = ('', '')
 
 # CAS login configuration
 CAS_EXTRA_LOGIN_PARAMS = {
@@ -160,9 +110,3 @@ CAS_EXTRA_LOGIN_PARAMS = {
 CAS_SERVER_URL = 'https://sistemas.ufsc.br/'
 CAS_USERNAME_ATTRIBUTE = 'idPessoa'
 CAS_VERSION = 'CAS_2_SAML_1_0'
-
-# Payment gateway configuration
-PAGSEGURO_EMAIL = 'dce@contato.ufsc.br'
-PAGSEGURO_TOKEN = ''
-PAGSEGURO_SANDBOX = True
-PAGSEGURO_LOG_IN_MODEL = True
