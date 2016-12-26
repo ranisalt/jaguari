6015ee57138355a3f8774e34c1fed9a9c2449683
Ranieri Althoff <ranisalt@gmail.com>
2016-12-26 04:34:19 -0200

    Namespace urls for app isolation

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from .views import OrderCreateView, OrderDetailView, OrdersView
 
 urlpatterns = [
-    url(r'^$', OrdersView.as_view(), name='orders'),
-    url(r'^(?P<pk>[\w-]{36})/$', OrderDetailView.as_view(), name='order-detail'),
-    url(r'^new/$', OrderCreateView.as_view(), name='order-new'),
+    url(r'^$', OrdersView.as_view(), name='index'),
+    url(r'^(?P<pk>[\w-]{36})/$', OrderDetailView.as_view(), name='detail'),
+    url(r'^new/$', OrderCreateView.as_view(), name='create'),
     url(r'^callback/', include('pagseguro.urls')),
 ]
