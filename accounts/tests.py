5b83065b2c7a0892a1b72ab582296ec2e2341a8d
Ranieri Althoff <ranisalt@gmail.com>
2017-03-06 03:34:07 -0300

    Hotfix for overly long names
    Former-commit-id: d6380ac9b8cf3033b9d2b699687d556519715101

--- accounts/tests.py
+++ accounts/tests.py
@@ -30,6 +30,21 @@ class Accounts(TransactionTestCase):
 
         user = User.objects.get()
         self.assertEqual('100000000400000', user.get_username())
-        self.assertEqual('John', user.first_name)
-        self.assertEqual('Edward Gammell', user.last_name)
+        self.assertEqual('John Edward', user.first_name)
+        self.assertEqual('Gammell', user.last_name)
         self.assertEqual('john@gmail.com', user.email)
+
+    def test_cas_login_long_name(self):
+        req = HttpRequest()
+        req.session = {}
+        with responses.RequestsMock() as r, resource('login-longname.xml',
+                                                     'rb') as body:
+            r.add(responses.POST, 'https://sistemas.ufsc.br/samlValidate',
+                  body=body.read())
+            self.client.login(ticket='ST-{}'.format(uuid.uuid4()),
+                              service=settings.CAS_SERVER_URL,
+                              request=req)
+
+        user = User.objects.get()
+        self.assertEqual('Carolina Josefa Leopoldina de', user.first_name)
+        self.assertEqual('Habsburgo-Lorena', user.last_name)
