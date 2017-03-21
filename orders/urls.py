87716aebc30a0274ce57e48e08dce7ed61e50e73
Ranieri Althoff <ranisalt@gmail.com>
2017-03-21 11:31:25 -0300

    Add app_name to modules (fix deprecation warning)

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from .views import OrderCreateView, OrderDetailView, OrdersView
 
+app_name = 'orders'
 urlpatterns = [
     url(r'^$', OrdersView.as_view(), name='index'),
     url(r'^(?P<pk>[\w-]{36})/$', OrderDetailView.as_view(), name='detail'),
