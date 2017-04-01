abc61d6118bd64424bc4e05cffaf268b15ddc500
Ranieri Althoff <ranisalt@gmail.com>
2017-04-01 03:57:36 -0300

    Replace CSS preview with SVG, add template download to admin panel
    Former-commit-id: 6577b789dbfe937bb1df302371236aed78f7ec73

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
-from .views import OrderCreateView, OrderDetailView, OrdersView
+from .views import OrderCreateView, OrderDetailView, OrderQrView, OrdersView
 
 app_name = 'orders'
 urlpatterns = [
     url(r'^$', OrdersView.as_view(), name='index'),
-    url(r'^(?P<pk>[\w-]{36})/$', OrderDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[\w-]{36}).svg$', OrderDetailView.as_view(), name='detail'),
+    url(r'^qr/(?P<slug>[\w-]{8}).svg$', OrderQrView.as_view(), name='qr'),
     url(r'^new/$', OrderCreateView.as_view(), name='create'),
     url(r'^callback/', include('pagseguro.urls')),
 ]
